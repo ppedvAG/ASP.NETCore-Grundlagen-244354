07a80594023ebb72bc8c95c2f4b8928401535d58
Ortwin Carstocea <OrtwinC@ppedv.de>
2025-02-10 11:35:01 +0100

    Lab 002 Loesung

--- LabIoC/Controllers/TestController.cs
+++ LabIoC/Controllers/TestController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Lab002_DependencyInjection.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LabIoC.Controllers
@@ -7,10 +8,40 @@ namespace LabIoC.Controllers
     [ApiController]
     public class TestController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult Index()
+        private readonly ILogger<TestController> _logger;
+        private readonly IOperationScoped _scoped;
+        private readonly IOperationTransient _transient;
+        private readonly IOperationSingleton _singleton;
+
+        public TestController(
+            ILogger<TestController> logger,
+            IOperationScoped scoped,
+            IOperationTransient transient,
+            IOperationSingleton singleton
+        )
         {
-            return Ok("Hello World");
+            _logger = logger;
+            _scoped = scoped;
+            _transient = transient;
+            _singleton = singleton;
+        }
+
+        [HttpGet]
+        [Route("/")]
+        public IActionResult Index(
+            [FromServices] IOperationScoped scoped,
+            [FromServices] IOperationTransient transient,
+            [FromServices] IOperationSingleton singleton
+            )
+        {
+            _logger.LogInformation("Transient\t\t{Transient}", _transient.OperationId);
+            _logger.LogInformation("Transient (local)\t{Transient}", transient.OperationId);
+            _logger.LogInformation("Scoped\t\t{Scoped}", _scoped.OperationId);
+            _logger.LogInformation("Scoped (local)\t{Scoped}", scoped.OperationId);
+            _logger.LogInformation("Singleton\t\t{Singleton}", _singleton.OperationId);
+            _logger.LogInformation("Singleton (local)\t{Singleton}", singleton.OperationId);
+
+            return Ok("Check logs in the console");
         }
     }
 }
--- LabIoC/Program.cs
+++ LabIoC/Program.cs
@@ -1,3 +1,5 @@
+using Lab002_DependencyInjection.Services;
+
 namespace LabIoC
 {
     public class Program
@@ -5,6 +7,10 @@ namespace LabIoC
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddSingleton<IOperationSingleton, OperationService>();
+            builder.Services.AddScoped<IOperationScoped, OperationService>();
+            builder.Services.AddTransient<IOperationTransient, OperationService>();
 
             // Asp.Net sucht nach allen Klassen im Verzeichnis Controllers welche mit dem Suffix Controller aufhoeren
             builder.Services.AddControllers();
