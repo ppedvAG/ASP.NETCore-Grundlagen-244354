cfcdf8b53b43c2d6314f975ea7c8f401201314fb
Ortwin Carstocea <OrtwinC@ppedv.de>
2025-02-10 14:02:43 +0100

    Recipe Business Model + Tests

---
+++ BusinessModel.Tests/Tests/RecipeReaderTests.cs
@@ -0,0 +1,22 @@
+﻿using BusinessModel.Data;
+
+namespace BusinessModel.Tests.Tests
+{
+    [TestClass]
+    public sealed class RecipeReaderTests
+    {
+        /// <summary>
+        /// Benamung: ZuTestendeMethode_Szenario_ErwartetesErgebnis
+        /// </summary>
+        [TestMethod]
+        public void FromJsonFile_ReadFromFile_ReturnsListOfRecipes()
+        {
+            var result = RecipeReader.FromJsonFile();
+
+            // Assert
+            Assert.IsNotNull(result, "Die Liste mit Rezepten sollte nicht null sein.");
+            Assert.IsTrue(result.Count > 0, "Die Liste mit Rezepten sollte nicht leer sein.");
+            Assert.IsTrue(result[0].MealType.Length > 0, "Das erste Rezept sollte einen MealTyp haben.");
+        }
+    }
+}
---
+++ BusinessModel.Tests/Tests/RecipeServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using BusinessModel.Services;
+
+namespace BusinessModel.Tests.Tests
+{
+    [TestClass]
+    public class RecipeServiceTests
+    {
+
+        /// <summary>
+        /// Benamung: ZuTestendeMethode_Szenario_ErwartetesErgebnis
+        /// </summary>
+        [TestMethod]
+        public void GetAll_Default_ReturnsListOfRecipes()
+        {
+            // Arrange
+            var recipeService = new SimpleRecipeService();
+
+            // Act
+            var result = recipeService.GetAll();
+
+            // Assert
+            Assert.IsNotNull(result, "Die Liste mit Rezepten sollte nicht null sein.");
+            Assert.IsTrue(result.Count > 0, "Die Liste mit Rezepten sollte nicht leer sein.");
+            Assert.IsTrue(result[0].MealType.Length > 0, "Das erste Rezept sollte einen MealTyp haben.");
+        }
+
+        // Hier koennten noch weitere Testszenarien ergaenzt werden fuer alle Methoden
+        // Gut- als auch schlechter Testfall, z. B. Test mit gueltiger als auch ungueltiger Id
+    }
+}
